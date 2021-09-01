419dda1deddebf5ea6852dc35f26ed3eace0a3a2
david-xander <ddelgado@a2sistemas.com>
2021-09-01 10:18:51 +0200

    fixes in the input and readme

---
+++ stack calc operations in paterentheses/calcparentheses.py
@@ -0,0 +1,50 @@
+from datastructures import Stack
+import re
+
+class Calc:
+    def __init__(self, input) -> None:
+        self.input = input
+
+    def __eq__(self, o: object) -> bool:
+        return self.result() == o.result()
+
+    def result(self) -> int:
+        input = self.input
+        res = 0
+        stack = Stack()
+        stack_parentheses = Stack()
+        grouping_start=['{','[','(']
+        grouping_end=['}',']',')']
+        for char in input:
+            if char in grouping_start:
+                stack_parentheses.push(char)
+            elif char in grouping_end:
+                actual = grouping_start[grouping_end.index(char)]
+                previous = stack_parentheses.pop()
+                if actual == previous:
+                    operand2 = stack.pop()
+                    operator = stack.pop()
+                    operand1 = stack.pop()
+                    res = self.operate(operand1, operand2, operator)
+                    stack.push(str(res))
+                else:
+                    raise ValueError('Invalid input')
+            else:
+                stack.push(char)
+
+        return res
+    
+    def operate(self, operand1, operand2, operator):
+        res = 0
+        if operator == '+':
+            res = int(operand1)+int(operand2)
+        elif operator == '-':
+            res = int(operand1)-int(operand2)
+        elif operator == '*':
+            res = int(operand1)*int(operand2)
+        elif operator == '/':
+            res = int(operand1)//int(operand2)                        
+        return res
+
+if __name__ == '__main__':
+    print(Calc('{30/[5*(1+2)]}').result())
---
+++ stack calc operations in paterentheses/readme.md
@@ -0,0 +1,16 @@
+## Pracice of stack data structure
+Simple calculator
+
+1. No testing of input. Is assumed OK
+    - numbers should be under 10
+
+2. In the second round, write the test for the input and support for:
+    - 2 digits numbers and more
+    - numbers under 10 should throw exception
+    - valid groupers are: {,[,{
+    - allowed operations: +,-,*,/
+
+
+BALANCED PARENTHESES:
+
+[(5+6)*(7+8)]/(4+3)
